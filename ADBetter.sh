b7258511740988c0e18a2cab418bb2e3ab0a1e9e
Blackl1st <gplocal@celop-eop251.bu.edu>
2015-06-22 10:17:55 -0400

    v3

--- ADBetter.sh
+++ ADBetter.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
- 
-computerid=`/usr/sbin/scutil --get LocalHostName`
 
 # Standard parameters
+printf "What do you want to call this computer? "
+read computerid
+sudo scutil --set LocalHostName "$computerid"
 domain="ad.bu.edu"               # fully qualified DNS name of Active Directory Domain
 printf "Type Username of a privileged network user: "
 read udn
@@ -12,8 +13,26 @@ read -s password
 #password="password"                         # password of a privileged network user
 ou="CN=Computers,DC=domain,DC=school,DC=edu"          # Distinguished name of container for the computer
 
+# Advanced options
+alldomains="enable"               # 'enable' or 'disable' automatic multi-domain authentication
+localhome="enable"               # 'enable' or 'disable' force home directory to local drive
+protocol="smb"                    # 'afp' or 'smb' change how home is mounted from server
+mobile="disable"               # 'enable' or 'disable' mobile account support for offline logon
+mobileconfirm="disable"          # 'enable' or 'disable' warn the user that a mobile acct will be created
+useuncpath="disable"               # 'enable' or 'disable' use AD SMBHome attribute to determine the home dir
+#UID="bu-ph-index-id-numeric"
+user_shell="/bin/bash"          # e.g., /bin/bash or "none"
+preferred="-nopreferred"     # Use the specified server for all Directory lookups and authentication
+# (e.g. "-nopreferred" or "-preferred ad.server.edu")
+admingroups="AD\celop_adms, AD\celop_senior_admissions_coordinator"     # These comma-separated AD groups may administer the machine (e.g. "" or "APPLE\mac admins")
 
-dsconfigad -f -a $computerid -domain $domain -u $udn -p "$password" -ou "$ou" -uid "bu-ph-index-id-numeric"
+# Activate the AD plugin
+defaults write /Library/Preferences/DirectoryService/DirectoryService "Active Directory" "Active"
+plutil -convert xml1 /Library/Preferences/DirectoryService/DirectoryService.plist
+sleep 5
+
+# Bind to AD
+dsconfigad -f -a "$computerid" -domain $domain -u $udn -p "$password" -ou "$ou" -uid "bu-ph-index-id-numeric"
 
 # Configure advanced AD plugin options
 if [ "$admingroups" = "" ]; then
@@ -28,3 +47,12 @@ dsconfigad -alldomains $alldomains -localhome $localhome -protocol $protocol \
  
 # Restart DirectoryService (necessary to reload AD plugin activation settings)
 killall DirectoryService
+
+# Add the AD node to the search path
+if [ "$alldomains" = "enable" ]; then
+csp="/Active Directory/All Domains"
+else
+csp="/Active Directory/$domain"
+fi
+
+dscl /Active\ Directory/All\ Domains -list /Users
